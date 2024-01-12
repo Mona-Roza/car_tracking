804d022b50ca14b8dffbb4e7a6ace5928d78ffc6
Mustafa Çelik <mustafa_celik2001@hotmail.com>
2024-01-12 16:24:11 +0300

    added certificate location

---
+++ web_app/DeneyKonsolu/Program.cs
@@ -0,0 +1,16 @@
+﻿using NLayer.Core.Data;
+class Program { static async Task Main() { var app = new ApplicationManager(); await Task.WhenAll(app.Run()); } }
+public class ApplicationManager
+{
+    public ApplicationManager()
+    {
+
+    }
+
+    public async Task Run()
+    {
+        
+
+    }
+
+}
---
+++ web_app/NLayer.Core/Models/MqttMessageModel.cs
@@ -0,0 +1,14 @@
+﻿namespace NLayer.Core.Models
+{
+    public class MqttMessageModel:EventArgs
+    {
+        public string MessagePayload { get; set; }
+        public string Topic { get; set; }
+
+        public MqttMessageModel(string topic, string messagePayload)
+        {
+            Topic = topic;
+            MessagePayload = messagePayload;
+        }
+    }
+}
