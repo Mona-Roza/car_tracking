db96ebfcd5dccef2e0520328164bd9299475206f
Mona-Roza <basaknisani@gmail.com>
2024-04-15 07:14:59 +0300

    .

--- embedded_app/application/main/main.cpp
+++ embedded_app/application/main/main.cpp
@@ -20,9 +20,9 @@ void app_main(void) {
 
 	// MORO_SIM800L::register_sim_card("2278", 10000, 2);
 
-	uint8_t rssi;
-	uint8_t ber;
-	// esp_err_t ret = MORO_SIM800L::get_signal_quality(&rssi, &ber, 10000, 2);
+	// uint8_t rssi;
+	// uint8_t ber;
+	// esp_err_t ret = MORO_SIM800L::get_signal_quality(&rssi, &ber, 100000, 2);
 
-	// esp_err_t ret = MORO_SIM800L::send_sms("+905312167900", "Hello World", NULL, 10000, 2);
+	// MORO_SIM800L::send_sms("+905312167900", "Hello World", NULL, 100000, 2);
 }
